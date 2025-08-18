320af5df6b95c5c7506e76411c6e6a3b53903284
pacha-tech <armel8594@gmail.com>
2025-08-18 17:41:58 +0100

    implementation de BookService et de quelques controller

--- src/main/java/com/tp/controller/books/AddBookController.java
+++ src/main/java/com/tp/controller/books/AddBookController.java
@@ -1,5 +1,9 @@
 package com.tp.controller.books;
 
+import com.tp.model.Book;
+import com.tp.model.generateID.GenerateIdBooks;
+import com.tp.service.BookService;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -7,10 +11,32 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class AddBookController extends HttpServlet {
-    protected void doGet(HttpServletRequest request , HttpServletResponse response ) throws ServletException , IOException {
 
+    BookService bookService = new BookService();
+    Book book = null;
+    GenerateIdBooks ID = new GenerateIdBooks();
+
+    protected void doGet(HttpServletRequest request , HttpServletResponse response ) throws ServletException , IOException {
+        this.getServletContext().getRequestDispatcher("WEB-INF/AddBook.jsp").forward(request,response);
     }
     protected void doPost(HttpServletRequest request , HttpServletResponse response ) throws ServletException , IOException {
+        book.setId_Book(ID.generateID());
+        book.setTitle(request.getParameter("title"));
+        book.setAuthor(request.getParameter("author"));
+        String yearString = request.getParameter("year");
+        book.setYear(Integer.parseInt(yearString));
+        book.setImage(request.getParameter("image"));
+        book.setCategory(request.getParameter("category"));
+        book.setDescription(request.getParameter("description"));
+        String is_available = request.getParameter("is_available");
+        book.setIs_available(Integer.parseInt(is_available));
+        String loan_count = request.getParameter("loan_count");
+        book.setLoan_count(Integer.parseInt(loan_count));
 
+        try {
+            bookService.addBook(book);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }

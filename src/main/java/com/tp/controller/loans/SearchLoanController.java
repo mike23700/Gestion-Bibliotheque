0583d37ee5e64869e824fdfadee31c02b9782591
pacha-tech <armel8594@gmail.com>
2025-08-19 17:18:39 +0100

    debut de l'implemntation du module loan

--- src/main/java/com/tp/controller/loans/SearchLoanController.java
+++ src/main/java/com/tp/controller/loans/SearchLoanController.java
@@ -1,4 +1,23 @@
 package com.tp.controller.loans;
 
-public class SearchLoanController {
+import com.tp.model.User;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+public class SearchLoanController extends HttpServlet {
+    protected void doGet(HttpServletRequest request , HttpServletResponse response) throws ServletException , IOException {
+        HttpSession session = request.getSession(false);
+        User currentUser = (session == null )? null : (User) session.getAttribute("user");
+
+        if(session == null || currentUser.getRole().equals("ADMIN")){
+            response.sendError(HttpServletResponse.SC_FORBIDDEN);
+            return;
+        }
+
+    }
 }
--- src/main/java/com/tp/dao/interfaces/LoanDAO.java
+++ src/main/java/com/tp/dao/interfaces/LoanDAO.java
@@ -1,4 +1,13 @@
 package com.tp.dao.interfaces;
 
+import com.tp.model.Loan;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface LoanDAO {
+    void AddLoan(Loan loan) throws Exception;
+    void DeleteLoan(String loan_id) throws Exception;
+    List<Loan> getAllLoans() throws Exception;
+    List<Loan> searchLoan(LocalDateTime date) throws Exception;
 }
